a4c924389f5a49b8b280cebbac3cf94844f01c9c
Gábor <wyth.nrg@gmail.com>
2022-08-27 22:51:36 +0200

    created aboutme file

---
+++ Profile/about.py
@@ -0,0 +1,7 @@
+informations = ["👀 I’m interested in everything in relation to AI",
+                "🌱 I’m currently learning DL algos :hurtrealbad: :goberserk:",
+                "🎯 2022 Goals: solo projects with [tensorflow] Tensorflow and [pytorch] Pytorch"]
+
+def aboutMe():
+    for info in informations:
+        print(info)
