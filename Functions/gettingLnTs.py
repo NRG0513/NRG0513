9ff750777f861fbf0f4e22b64baa7eb5cee15857
Gábor <wyth.nrg@gmail.com>
2022-08-27 19:21:23 +0200

    added functions to clear the notebook

---
+++ Functions/gettingLnTs.py
@@ -0,0 +1,23 @@
+from matplotlib.pyplot import figure, imshow, axis
+from matplotlib.image import imread
+
+compiledText = []
+
+def gettingLnTs():
+    listOfImgData =  [["code.visualstudio.com", "vscode-original", "vscode"],
+                      ["jupyter.org", "jupyter-original-wordmark", "jupyter"],
+                      ["www.python.org", "python-original", "python"],
+                      ["www.gnu.org/software/bash", "bash-original", "bash"],
+                      ["git-scm.com", "git-original", "git"],
+                      ["github.com", "github-original", "github"],
+                      ["numpy.org", "numpy-original", "numpy"],
+                      ["www.tensorflow.org", "tensorflow-original", "tensorflow"],
+                      ["pytorch.org", "pytorch-original", "pytorch"]]
+    
+    for i in range(0, len(listOfImgData)):
+        
+        htmlCode = f"https://raw.githubusercontent.com/NRG0513/NRG0513/main/Images/png/{listOfImgData[i][1]}.png"
+        
+        compiledText.append(htmlCode)
+        
+    return compiledText
