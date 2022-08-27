20858130880c9a265fd48c6cbd879c9a5597288b
Gábor <wyth.nrg@gmail.com>
2022-08-27 20:14:16 +0200

    updated file

--- Profile/intro.py
+++ Profile/intro.py
@@ -3,4 +3,4 @@ import random
 def introduction():
     name = ["Gábor", "Gabe", "Gabriel", "NRG"]
     name_set = random.choice(name)
-    return f"Hi, 👋 I’m {name_set}"
+    print(f"Hi, 👋 I’m {name_set}")
